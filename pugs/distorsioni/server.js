1146c6809926618c9ea61045b1d627636cf885a0
Claudio Agosti <claudio@tracking.exposed>
2018-01-19 12:26:07 +0100

    progressi nella trasformazione dei post in qualcosa di tabellabile

--- pugs/distorsioni/server.js
+++ pugs/distorsioni/server.js
@@ -2,19 +2,12 @@ var _ = require('lodash');
 var debug = require('debug')('server:distorsioni');
 var pug = require('pug');
 var nconf = require('nconf').env();
-var querystring = require('querystring');
-var mongo = require('../../../../lib/mongo')
+var moment = require('moment');
+var Promise = require('bluebird');
+var mongo = require('../../../../lib/mongo');
+var various = require('../../../../lib/various');
 
-var collectionName = 'fbtposts';
-var fullp = __dirname + '/' + 'distorsioni.pug';
 
-function distorsioni(req) {
-
-    var pageName = _.get(req.params, 'page');
-    debug("page request for: %s", pageName);
-
-    /* si faranno esperimenti con sharding, backup o ridondanze varie */
-    mongo.forcedDBURL = 'mongodb://localhost/e18';
 /*
   { _id: { _bsontype: 'ObjectID', id: [Object] },
     postId: '1690449564331296',
@@ -35,21 +28,76 @@ function distorsioni(req) {
     appears: [ [Object] ] },
 
   */
-debugger;
-    return mongo
-        .read(collectionName, {})
-        .then(function(k) {
 
+function distorsioni(req) {
 
+    var daysago = 0;
+    var collectionName = 'fbtposts';
+    var fullp = __dirname + '/' + 'distorsioni.pug';
+    var usersf = __dirname + '/../../fonti/utenti-exp1.json';
+    mongo.forcedDBURL = 'mongodb://localhost/e18';
 
-            var tablecontent = querystring.stringify(k);
-            var x = encodeURI(JSON.stringify(k));
-            debugger;
-            return { 'text': pug.compileFile(fullp, {
-                    pretty: true,
-                    debug: false
-                })({
-                    tablecontent: x
+    /* anzichè accedere a posts si dovrebbe prendere un merge di:
+     * i post pubblicati nel range 24 ore - 48 ore fa
+     * ordinati per maggiore engagement (oppure)
+     * ordinati per avere un post per fonte
+     * scaricata immagine di preview
+     * analisi semantica
+     * analisi dei trackers
+     * analisi delle fonti per mostrare la "competizione"
+     * tutto insieme a creare questo pacchetto informativo
+     */
+    var min = moment().subtract(daysago +2, 'd').format("YYYY-MM-DD");
+    var max = moment().subtract(daysago +1, 'd').format("YYYY-MM-DD");
+    var filter = {
+        publicationTime: { '$gt': new Date(min), '$lt': new Date(max) }
+    };
+
+    return Promise.all([
+            mongo
+                .readLimit(collectionName, filter, {}, 1000, 0),
+            various
+                .loadJSONfile(usersf)
+        ])
+        .then(function(mix) {
+
+            /* posts */
+            debug("With a maximum posts of 1000, retrieved %d", _.size(mix[0]));
+
+            /* select pages, only the first 15 producers */
+            var pageSelected = _.take(_.orderBy(_.map(_.countBy(mix[0], 'pageName'), function(occ, pageName) {
+                return { occurrency: occ, pageName: pageName };
+            }), 'occurrency', 'desc'), 15);
+
+            /* pick the most five seen posts for every page */
+            var topinteracted = _.reduce(_.groupBy(mix[0], 'pageName'), function(memo, l, p) {
+                var c = {
+                    posts: _.take(_.orderBy(l, function(o) { return _.size(o.appears); }, 'desc'), 5),
+                    pageName: p,
+                    totals: _.size(l)
+                }
+                memo.push(c);
+                return memo;
+            }, []);
+
+            /* keep only the posts selected belonging to the right pages */
+            var selection = _.filter(topinteracted, function(o) {
+                return _.find(pageSelected, { pageName: o.pageName });
+            });
+
+            var postlist = encodeURI(JSON.stringify(selection));
+
+            /* users */
+            var userinfo = encodeURI(JSON.stringify(mix[1]));
+
+            /* HTTP render */
+            return {
+                'text': pug.compileFile(
+                            fullp,
+                            { pretty: true, debug: false }
+                        )({
+                            postsmap: postlist,
+                            users: userinfo
                 })
             };
         });
--- web-accessible/e18.js
+++ web-accessible/e18.js
@@ -1,14 +1,21 @@
-function generateDistortionTable(clean) {
+function generateDistortionTable(posts, users) {
+
     /* this function return a string of HTML containing a table */
+
 
 };
 
-function loadDistorsions(containerId, content) {
+// append/update, reminder
+// https://stackoverflow.com/questions/171027/add-table-row-in-jquery
+//    $('#myTable tr:last').after('<tr>...</tr><tr>...</tr>');
+
+function loadDistorsions(containerId, stringcnt, stringusers) {
 
     console.log("Loading as JSON string the second argument");
-    var clean = JSON.parse(content);
+    var posts = JSON.parse(decodeURI(stringcnt));
+    var users = JSON.parse(decodeURI(stringusers));
     console.log(containerId);
-    console.log(clean);
+    console.log(posts);
 
-    $(containerId).html(generateDistorsionTable(clean));
+    $(containerId).html(generateDistortionTable(posts, users));
 };
