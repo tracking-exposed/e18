2c5a4f9933af78854b9745b481eff649fbf8aa13
vecna <claudio@tracking.exposed>
2018-02-05 22:36:38 +0000

    fixato errore in index.js

--- pugs/index.js
+++ pugs/index.js
@@ -4,8 +4,8 @@ module.exports = {
     sponsorizzati: require('./sponsorizzati/server.js'),
     dibattito: require('./dibattito/server.js'),
     sorveglianza: require('./sorveglianza/server.js'),
-    partecipare: require('./partecipare/server.js'),
 
+    partecipare: require('./statiche/server.js'),
     contatti: require('./statiche/server.js'),
     filosofia: require('./statiche/server.js'),
     soldi: require('./statiche/server.js')
