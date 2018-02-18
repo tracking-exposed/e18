5e8dbd923788c91f30e2cfec8d3cfe0f48210b4f
Claudio <claudio@tracking.exposed>
2018-02-18 01:05:29 +0100

    passando a reveal, diverso stile, ma almeno mobile funziona

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,2 +1,20 @@
 ../../node_modules/.bin/stylus web-accessible/base.styl -o web-accessible/
 ../../node_modules/.bin/stylus web-accessible/carousel-styles.styl -o web-accessible/
+npm i
+
+cp -r node_modules/reveal.js/js web-accessible/reveal-js
+echo "copy of node_modules/reveal.js/js to web-accessible/reveal-js"
+
+cp -r node_modules/reveal.js/css web-accessible/reveal-css
+echo "copy of node_modules/reveal.js/css web-accessible/reveal-css"
+
+cp -r node_modules/reveal.js/css web-accessible/reveal-css
+echo "copy of node_modules/reveal.js/css web-accessible/reveal-css"
+
+mkdir -p web-accessible/reveal-js/lib
+cp node_modules/reveal.js/lib/js/* web-accessible/reveal-js/lib
+echo "copy of node_modules/reveal.js/lib/js/* to web-accessible/reveal-js/lib"
+
+mkdir -p web-accessible/lib/font
+cp -r node_modules/reveal.js/lib/font web-accessible/lib/font
+echo "copy of node_modules/reveal.js/lib/font/* to web-accessible/lib/font"
--- web-accessible/carousel-fix.js
+++
@@ -1,6 +0,0 @@
-/*
-$(document).ready(function() {
-    console.log("done?")
-    jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 500;
-}
-*/
